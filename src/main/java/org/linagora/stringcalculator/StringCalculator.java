524f975ec69adeefe812e81f7cce352039912a0b
Laura Royet <lroyet@laura>
2015-12-10 11:51:11 +0100

    Java 7

--- src/main/java/org/linagora/stringcalculator/StringCalculator.java
+++ src/main/java/org/linagora/stringcalculator/StringCalculator.java
@@ -2,6 +2,24 @@ package org.linagora.stringcalculator;
 
 public class StringCalculator {
 
-    public int calculate(String valueAsString) {
+    public int calculate(String valueAsString)throws IllegalArgumentException {
+        if (valueAsString == null || valueAsString.isEmpty()  ){
+            return 0;
+        }
+        String[] numbers = valueAsString.split(",|\n");
+
+        int sum = 0;
+        for(String number : numbers){
+            int result = Integer.valueOf(number);
+            if(result < 0){
+                throw new IllegalArgumentException();
+            }
+            if (result > 1000){
+                continue;
+            }
+            sum +=  result;
+        }
+
+        return sum;
     }
 }
--- src/test/java/org/linagora/stringcalculator/StringCalculatorTest.java
+++ src/test/java/org/linagora/stringcalculator/StringCalculatorTest.java
@@ -1,5 +1,10 @@
 package org.linagora.stringcalculator;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.Before;
+import org.junit.Test;
+
 public class StringCalculatorTest {
 
     /**
@@ -8,7 +13,61 @@ public class StringCalculatorTest {
     *    3. Two numbers, comma delimited, returns the sum ("3,4" -> 7)
     *    4. Two numbers, newline delimited, returns the sum ("3\n4" -> 7)
     *    5. Three numbers, delimited either way, returns the sum ("3,4\n2" -> 9)
-    *    6. Negative numbers throw an exception ("3,4\-2" -> Exception)
+    *    6. Negative numbers throw an exception ("3,4\n-2" -> Exception)
     *    7. Numbers greater than 1000 are ignored ("3,4\n1002" -> 7)
     **/
+
+    private StringCalculator testee ;
+
+    @Before
+    public void setUp(){
+        testee = new StringCalculator() ;
+    }
+
+    @Test
+    public void calculatorShouldReturnZeroWhenEmptyString(){
+        int value = testee.calculate("") ;
+        assertThat(value).isEqualTo(0);
+    }
+
+    @Test
+    public void calculatorShouldReturnZeroWhenNullString(){
+        int value = testee.calculate(null) ;
+        assertThat(value).isEqualTo(0);
+    }
+
+    @Test
+    public void calculatorShouldReturnValueWhenSingleNumber(){
+        int value = testee.calculate("3") ;
+        assertThat(value).isEqualTo(3);
+    }
+
+    @Test
+    public void calculatorShouldReturnSumWhenCommaDelimited(){
+        int value = testee.calculate("3,4") ;
+        assertThat(value).isEqualTo(7);
+    }
+
+    @Test
+    public void calculatorShouldReturnSumWhenNewlineDelimited(){
+        int value = testee.calculate("3\n4") ;
+        assertThat(value).isEqualTo(7);
+    }
+
+    @Test
+    public void calculatorShouldReturnSumWhenEitherWayDelimited(){
+        int value = testee.calculate("3,4\n2") ;
+        assertThat(value).isEqualTo(9);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void calculatorShouldThrowWhenNegativeNumber(){
+        testee.calculate("3,4\n-2") ;
+    }
+
+    @Test
+    public void calculatorShouldIgnoreWhenNumberGreaterThan1000(){
+        int value = testee.calculate("3,4\n1002") ;
+        assertThat(value).isEqualTo(7);
+    }
 }
